461b0e4f23ecaf089a6477bc19ff2e5bd3ee6d2d
Isaac150327 <150327@lrstudents.org>
2025-10-30 16:23:05 -0400

    actual subsystem template fr
    buggy

---
+++ SubsystemIO.java
@@ -0,0 +1,10 @@
+public interface <subsystem>IO {
+	public default (just the methods already done in gripper) {
+		}
+
+	
+	public default void updateInputs(<Subsystem>IOInputs inputs) {}
+}
+
+		
+}
